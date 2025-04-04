7b28b151b5efed1e05ad074fe4ad527e58fa9892
missinggoverner <zekariyasamdu24@gmail.com>
2025-04-04 13:33:36 +0300

    oh look i can like a post

--- UI/pages/home/app.js
+++ UI/pages/home/app.js
@@ -1,8 +1,7 @@
 let content = document.querySelector(".content");
 let greeting = document.querySelector(".greeting");
-let home =document.querySelector('#home')
+let home = document.querySelector('#home')
 let myPosts = document.querySelector('.my-posts');
-let noPosts = document.querySelector('.no-posts');
 
 let jwtToken = localStorage.getItem('token');
 let user = localStorage.getItem('user');
@@ -10,113 +9,180 @@ let user = localStorage.getItem('user');
 let moment = require('moment');
 
 // browserify UI/pages/home/app.js -o bundle.js
+// npm run watch
+// npm run dev
 
 const placement = (posts) => {
 
-            // Each post container
-            let post = document.createElement("div");
-            post.className = "post-container"; 
-            content.appendChild(post);
+    // Each post container
+    let post = document.createElement("div");
+    post.className = "post-container";
+    content.appendChild(post);
 
-            // Post Header
-            let header = document.createElement("div");
-            header.className = "header"; 
-            post.appendChild(header);
+    // Post Header
+    let header = document.createElement("div");
+    header.className = "header";
+    post.appendChild(header);
 
-            // User Name
-            let user_name = document.createElement("div");
-            user_name.className = "user-name"; 
-            user_name.innerHTML = `By:@${posts.Post.user_name}`;
-            header.appendChild(user_name);
+    // User Name
+    let user_name = document.createElement("div");
+    user_name.className = "user-name";
+    user_name.innerHTML = `By:@${posts.Post.user_name}`;
+    header.appendChild(user_name);
 
-            // Date
-            let date = document.createElement("div");
-            date.className = "date"; 
-            date.innerHTML = `Posted ${moment(posts.Post.created_at).fromNow()}`;
-            header.appendChild(date);
+    // Date
+    let date = document.createElement("div");
+    date.className = "date";
+    date.innerHTML = `Posted ${moment(posts.Post.created_at).fromNow()}`;
+    header.appendChild(date);
 
-            // Post Title
-            let title = document.createElement("div");
-            title.className = "title"; 
-            title.innerHTML = `<strong>${posts.Post.title}</strong>`;
-            post.appendChild(title);
+    // Post Title
+    let title = document.createElement("div");
+    title.className = "title";
+    title.innerHTML = `<strong>${posts.Post.title}</strong>`;
+    post.appendChild(title);
 
-            // Post text
-            let text = document.createElement("div");
-            text.className = "text"; 
-            text.innerHTML = posts.Post.content;
-            post.appendChild(text); 
+    // Post text
+    let text = document.createElement("div");
+    text.className = "text";
+    text.innerHTML = posts.Post.content;
+    post.appendChild(text);
 
-            // Vote Container
-            let vote_container = document.createElement("div");
-            vote_container.className = "vote-container"; 
-            post.appendChild(vote_container);
+    // Vote Container
+    let vote_container = document.createElement("div");
+    vote_container.className = "vote-container";
+    post.appendChild(vote_container);
 
-            // Votes
-            let votes = document.createElement("div");
-            votes.className = "votes"; 
-            votes.innerHTML = posts.votes;
-            vote_container.appendChild(votes);
+    // Votes
+    let votes = document.createElement("div");
+    votes.className = "votes";
+    votes.innerHTML = posts.votes;
+    vote_container.appendChild(votes);
 
-            // Vote Button
-            let vote_button = document.createElement("button");
-            vote_button.className = "vote-button"; 
-            vote_container.appendChild(vote_button);
+    // Voteup
+    let voteup = document.createElement("button");
+    voteup.className = "voteup vote-button";
+    vote_container.appendChild(voteup);
 
-            // Vote icon
-            let vote_icon = document.createElement("i");
-            vote_icon.className = "fa-regular fa-heart"; 
-            vote_button.appendChild(vote_icon);
+    // Votedown
+    let votedown = document.createElement("button");
+    votedown.className = "votedown vote-button";
+    vote_container.appendChild(votedown);
 
+    // id
+    let id = document.createElement("div");
+    id.className = "id";
+    id.innerHTML = posts.Post.id;
+    vote_container.appendChild(id);
+
+    // Vote icon
+    let vote_icon = document.createElement("i");
+    vote_icon.className = "fa-regular fa-heart";
+    voteup.appendChild(vote_icon);
+
+    // Vote icon2
+    let vote_icon2 = document.createElement("i");
+    vote_icon2.className = "fa-solid fa-heart-crack";
+    votedown.appendChild(vote_icon2);
+
+    voteup.addEventListener('click', async () => {
+        console.log('clicked');
+        const postId = id.innerHTML;
+        const sendData = {
+            post_id: postId,
+            dir: 1
+        }
+        try {
+            const response = await fetch('http://localhost:8000/vote/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${jwtToken}`
+                },
+                body: JSON.stringify(sendData)
+            });
+            if (!response.ok) {
+                const error = await response.json();
+                console.error('Error:', error.detail);
+            }
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    });
+
+    votedown.addEventListener('click', async () => {
+        console.log('clicked');
+        const postId = id.innerHTML;
+        const sendData = {
+            post_id: postId,
+            dir: 0
+        }
+        try {
+            const response = await fetch('http://localhost:8000/vote/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${jwtToken}`
+                },
+                body: JSON.stringify(sendData)
+            });
+            if (!response.ok) {
+                const error = await response.json();
+                console.error('Error:', error.detail);
+            }
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    });
 }
 
-const myPlacement = (posts) => { 
+const myPlacement = (posts) => {
 
     // Loop through each post in the array
     posts.forEach(post => {
         // Each post container
         let postContainer = document.createElement("div");
-        postContainer.className = "post-container"; 
+        postContainer.className = "post-container";
         myPosts.appendChild(postContainer);
 
         // Post Header
         let header = document.createElement("div");
-        header.className = "header"; 
+        header.className = "header";
         postContainer.appendChild(header);
 
         // Date
         let date = document.createElement("div");
-        date.className = "date"; 
+        date.className = "date";
         date.innerHTML = `Posted ${moment(post.Post.created_at).fromNow()}`;
         header.appendChild(date);
 
         // Post Title
         let title = document.createElement("div");
-        title.className = "title"; 
+        title.className = "title";
         title.innerHTML = `<strong>${post.Post.title}</strong>`;
         postContainer.appendChild(title);
 
         // Post text
         let text = document.createElement("div");
-        text.className = "text"; 
+        text.className = "text";
         text.innerHTML = post.Post.content;
-        postContainer.appendChild(text); 
+        postContainer.appendChild(text);
 
         // Vote Container
         let vote_container = document.createElement("div");
-        vote_container.className = "vote-container"; 
+        vote_container.className = "vote-container";
         postContainer.appendChild(vote_container);
 
         // Votes
         let votes = document.createElement("div");
-        votes.className = "votes"; 
+        votes.className = "votes";
         votes.innerHTML = `Total Votes: ${post.votes}`;
         vote_container.appendChild(votes);
     });
 }
 
 
-greeting.innerHTML = `Hello ${user}, Welcome!`;
+
 const getUserPosts = async () => {
     try {
         const response = await fetch('http://localhost:8000/posts/mine', {
@@ -129,14 +195,14 @@ const getUserPosts = async () => {
             const data = await response.json();
             if (data.length === 0) {
                 console.log('No posts found');
-                
+
             } else {
                 document.querySelector('.no-posts').style.display = 'none';
                 myPosts.innerHTML = '';
                 console.log(data);
                 myPlacement(data);
             }
-        } 
+        }
     } catch (error) {
         console.error('Fetch error:', error);
     }
@@ -144,7 +210,8 @@ const getUserPosts = async () => {
 getUserPosts();
 
 
-home.addEventListener('click', async () => {
+const homePage = async () => {
+    greeting.innerHTML = `Hello ${user}, Welcome!`;
     try {
         const response = await fetch('http://localhost:8000/posts/', {
             method: 'GET',
@@ -153,11 +220,9 @@ home.addEventListener('click', async () => {
         if (!response.ok) {
             const error = await response.json();
             console.error('Error:', error.detail);
-        } 
+        }
         const data = await response.json();
         console.log(data);
-        greeting.style.display = 'none';
-        content.innerHTML = '';
         data.forEach(posts => {
             placement(posts)
         });
@@ -165,9 +230,13 @@ home.addEventListener('click', async () => {
     } catch (error) {
         console.error('Fetch error:', error);
     }
-}); 
+}
 
+homePage();
 
+home.addEventListener('click', () => {
+    homePage();
+});
 
 const postNewPost = () => {
     return
--- bundle.js
+++ bundle.js
@@ -1,9 +1,8 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let content = document.querySelector(".content");
 let greeting = document.querySelector(".greeting");
-let home =document.querySelector('#home')
+let home = document.querySelector('#home')
 let myPosts = document.querySelector('.my-posts');
-let noPosts = document.querySelector('.no-posts');
 
 let jwtToken = localStorage.getItem('token');
 let user = localStorage.getItem('user');
@@ -11,113 +10,180 @@ let user = localStorage.getItem('user');
 let moment = require('moment');
 
 // browserify UI/pages/home/app.js -o bundle.js
+// npm run watch
+// npm run dev
 
 const placement = (posts) => {
 
-            // Each post container
-            let post = document.createElement("div");
-            post.className = "post-container"; 
-            content.appendChild(post);
+    // Each post container
+    let post = document.createElement("div");
+    post.className = "post-container";
+    content.appendChild(post);
 
-            // Post Header
-            let header = document.createElement("div");
-            header.className = "header"; 
-            post.appendChild(header);
+    // Post Header
+    let header = document.createElement("div");
+    header.className = "header";
+    post.appendChild(header);
 
-            // User Name
-            let user_name = document.createElement("div");
-            user_name.className = "user-name"; 
-            user_name.innerHTML = `By:@${posts.Post.user_name}`;
-            header.appendChild(user_name);
+    // User Name
+    let user_name = document.createElement("div");
+    user_name.className = "user-name";
+    user_name.innerHTML = `By:@${posts.Post.user_name}`;
+    header.appendChild(user_name);
 
-            // Date
-            let date = document.createElement("div");
-            date.className = "date"; 
-            date.innerHTML = `Posted ${moment(posts.Post.created_at).fromNow()}`;
-            header.appendChild(date);
+    // Date
+    let date = document.createElement("div");
+    date.className = "date";
+    date.innerHTML = `Posted ${moment(posts.Post.created_at).fromNow()}`;
+    header.appendChild(date);
 
-            // Post Title
-            let title = document.createElement("div");
-            title.className = "title"; 
-            title.innerHTML = `<strong>${posts.Post.title}</strong>`;
-            post.appendChild(title);
+    // Post Title
+    let title = document.createElement("div");
+    title.className = "title";
+    title.innerHTML = `<strong>${posts.Post.title}</strong>`;
+    post.appendChild(title);
 
-            // Post text
-            let text = document.createElement("div");
-            text.className = "text"; 
-            text.innerHTML = posts.Post.content;
-            post.appendChild(text); 
+    // Post text
+    let text = document.createElement("div");
+    text.className = "text";
+    text.innerHTML = posts.Post.content;
+    post.appendChild(text);
 
-            // Vote Container
-            let vote_container = document.createElement("div");
-            vote_container.className = "vote-container"; 
-            post.appendChild(vote_container);
+    // Vote Container
+    let vote_container = document.createElement("div");
+    vote_container.className = "vote-container";
+    post.appendChild(vote_container);
 
-            // Votes
-            let votes = document.createElement("div");
-            votes.className = "votes"; 
-            votes.innerHTML = posts.votes;
-            vote_container.appendChild(votes);
+    // Votes
+    let votes = document.createElement("div");
+    votes.className = "votes";
+    votes.innerHTML = posts.votes;
+    vote_container.appendChild(votes);
 
-            // Vote Button
-            let vote_button = document.createElement("button");
-            vote_button.className = "vote-button"; 
-            vote_container.appendChild(vote_button);
+    // Voteup
+    let voteup = document.createElement("button");
+    voteup.className = "voteup vote-button";
+    vote_container.appendChild(voteup);
 
-            // Vote icon
-            let vote_icon = document.createElement("i");
-            vote_icon.className = "fa-regular fa-heart"; 
-            vote_button.appendChild(vote_icon);
+    // Votedown
+    let votedown = document.createElement("button");
+    votedown.className = "votedown vote-button";
+    vote_container.appendChild(votedown);
 
+    // id
+    let id = document.createElement("div");
+    id.className = "id";
+    id.innerHTML = posts.Post.id;
+    vote_container.appendChild(id);
+
+    // Vote icon
+    let vote_icon = document.createElement("i");
+    vote_icon.className = "fa-regular fa-heart";
+    voteup.appendChild(vote_icon);
+
+    // Vote icon2
+    let vote_icon2 = document.createElement("i");
+    vote_icon2.className = "fa-solid fa-heart-crack";
+    votedown.appendChild(vote_icon2);
+
+    voteup.addEventListener('click', async () => {
+        console.log('clicked');
+        const postId = id.innerHTML;
+        const sendData = {
+            post_id: postId,
+            dir: 1
+        }
+        try {
+            const response = await fetch('http://localhost:8000/vote/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${jwtToken}`
+                },
+                body: JSON.stringify(sendData)
+            });
+            if (!response.ok) {
+                const error = await response.json();
+                console.error('Error:', error.detail);
+            }
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    });
+
+    votedown.addEventListener('click', async () => {
+        console.log('clicked');
+        const postId = id.innerHTML;
+        const sendData = {
+            post_id: postId,
+            dir: 0
+        }
+        try {
+            const response = await fetch('http://localhost:8000/vote/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${jwtToken}`
+                },
+                body: JSON.stringify(sendData)
+            });
+            if (!response.ok) {
+                const error = await response.json();
+                console.error('Error:', error.detail);
+            }
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    });
 }
 
-const myPlacement = (posts) => { 
+const myPlacement = (posts) => {
 
     // Loop through each post in the array
     posts.forEach(post => {
         // Each post container
         let postContainer = document.createElement("div");
-        postContainer.className = "post-container"; 
+        postContainer.className = "post-container";
         myPosts.appendChild(postContainer);
 
         // Post Header
         let header = document.createElement("div");
-        header.className = "header"; 
+        header.className = "header";
         postContainer.appendChild(header);
 
         // Date
         let date = document.createElement("div");
-        date.className = "date"; 
+        date.className = "date";
         date.innerHTML = `Posted ${moment(post.Post.created_at).fromNow()}`;
         header.appendChild(date);
 
         // Post Title
         let title = document.createElement("div");
-        title.className = "title"; 
+        title.className = "title";
         title.innerHTML = `<strong>${post.Post.title}</strong>`;
         postContainer.appendChild(title);
 
         // Post text
         let text = document.createElement("div");
-        text.className = "text"; 
+        text.className = "text";
         text.innerHTML = post.Post.content;
-        postContainer.appendChild(text); 
+        postContainer.appendChild(text);
 
         // Vote Container
         let vote_container = document.createElement("div");
-        vote_container.className = "vote-container"; 
+        vote_container.className = "vote-container";
         postContainer.appendChild(vote_container);
 
         // Votes
         let votes = document.createElement("div");
-        votes.className = "votes"; 
+        votes.className = "votes";
         votes.innerHTML = `Total Votes: ${post.votes}`;
         vote_container.appendChild(votes);
     });
 }
 
 
-greeting.innerHTML = `Hello ${user}, Welcome!`;
+
 const getUserPosts = async () => {
     try {
         const response = await fetch('http://localhost:8000/posts/mine', {
@@ -130,14 +196,14 @@ const getUserPosts = async () => {
             const data = await response.json();
             if (data.length === 0) {
                 console.log('No posts found');
-                
+
             } else {
                 document.querySelector('.no-posts').style.display = 'none';
                 myPosts.innerHTML = '';
                 console.log(data);
                 myPlacement(data);
             }
-        } 
+        }
     } catch (error) {
         console.error('Fetch error:', error);
     }
@@ -145,7 +211,8 @@ const getUserPosts = async () => {
 getUserPosts();
 
 
-home.addEventListener('click', async () => {
+const homePage = async () => {
+    greeting.innerHTML = `Hello ${user}, Welcome!`;
     try {
         const response = await fetch('http://localhost:8000/posts/', {
             method: 'GET',
@@ -154,11 +221,9 @@ home.addEventListener('click', async () => {
         if (!response.ok) {
             const error = await response.json();
             console.error('Error:', error.detail);
-        } 
+        }
         const data = await response.json();
         console.log(data);
-        greeting.style.display = 'none';
-        content.innerHTML = '';
         data.forEach(posts => {
             placement(posts)
         });
@@ -166,9 +231,13 @@ home.addEventListener('click', async () => {
     } catch (error) {
         console.error('Fetch error:', error);
     }
-}); 
+}
 
+homePage();
 
+home.addEventListener('click', () => {
+    homePage();
+});
 
 const postNewPost = () => {
     return
